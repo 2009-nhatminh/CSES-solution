80aa3d1b39d0e15c2020ca49deb341fdd349c7c2
2009_nhatminh <lenhatminh912@gmail.com>
2025-07-07 09:35:52 +0700

    Meow~~

---
+++ test.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+
+
+#define ll long long 
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fir first 
+#define sec second 
+#define piint pair < int , int >
+#define FOR( i , a , b ) for (int i = (a) , _b = (b) ; i <= _b ; i ++ )  
+#define FORD( i , a , b ) for (int i = (a) , _b = (b) ; i >= _b ; i -- )  
+
+#define pb push_back 
+#define str string 
+#define ALL(a) (a).begin() , (a).end() 
+#define rep( i , a , b) for (int i = (a) ; i < (b) ; i ++ )
+#define ld long double
+const int maxn = 1e3;
+#define debug 0
+#define oo (ll)(1e18)
+
+
+
+
+
+void input(){
+}
+void solve() {
+    ll n ; cin >> n ;
+    cout << n << ' ' ; 
+    while ( n != 1 ) {
+        if ( !(n & 1) ) {
+            n /= 2 ;
+        }
+        else n = n * 3 + 1 ;
+        cout << n << ' '; 
+    }
+}
+#define name "TASK" 
+int main(){
+    fast 
+    if(fopen(name".INP","r")) {
+        freopen (name".INP","r",stdin);
+        freopen (name".OUT","w",stdout);
+    }
+    input() ;
+    solve() ;
+    cerr << "\nTIME: = " << (1.0*clock())/CLOCKS_PER_SEC << '\n';
+    return(0) ;
+}
