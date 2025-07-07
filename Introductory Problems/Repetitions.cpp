1b3da919e8db1a8fe58d3638aa9634d183c25323
2009_nhatminh <lenhatminh912@gmail.com>
2025-07-07 10:52:52 +0700

    GOW GOW

---
+++ Introductory Problems/Repetitions.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+
+
+#define ll long long 
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fir first 
+#define sec second 
+#define piint pair < int , int >
+#define FOR( i , a , b ) for (int i = (a) , _b = (b) ; i <= _b ; i ++ )  
+#define FORD( i , a , b ) for (int i = (a) , _b = (b) ; i >= _b ; i -- )  
+
+#define pb push_back 
+#define str string 
+#define ALL(a) (a).begin() , (a).end() 
+#define rep( i , a , b) for (int i = (a) ; i < (b) ; i ++ )
+#define ld long double
+const int maxn = 1e3;
+#define debug 0
+#define oo (ll)(1e18)
+
+
+
+
+str s ; 
+void input(){
+    cin >> s ;
+
+}
+void solve() {
+    int maxx = -1 ;
+    int d = 1 ;
+    rep ( i , 1 , s.size()){
+        if ( s[i] == s[i-1] ){
+            d ++ ;
+        }
+        else d = 1 ; 
+        maxx = max ( d , maxx) ;
+    }
+    maxx = max ( maxx , d ) ;
+     
+    cout << maxx ; 
+
+}
+#define name "TASK" 
+int main(){
+    fast 
+    if(fopen(name".INP","r")) {
+        freopen (name".INP","r",stdin);
+        freopen (name".OUT","w",stdout);
+    }
+    input() ;
+    solve() ;
+    cerr << "\nTIME: = " << (1.0*clock())/CLOCKS_PER_SEC << '\n';
+    return(0) ;
+}
